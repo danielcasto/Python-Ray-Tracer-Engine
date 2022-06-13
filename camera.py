2f4dc1c474f8b3fbea9341c1baf2a64a913e953f
danielcasto <danielcasto242@gmail.com>
2022-06-12 19:30:27 -0700

    Added basic structure of repo

---
+++ camera.py
@@ -0,0 +1,11 @@
+class Camera:
+    def __init__(self):
+        pass
+
+class ParallelCamera(Camera):
+    def __init__(self):
+        super().__init__()
+
+class PerspectiveCamera(Camera):
+    def __init__(self):
+        super().__init__()
---
+++ light_source.py
@@ -0,0 +1,15 @@
+class LightSource:
+    def __init__(self):
+        pass
+
+class DirectionalInfinite(LightSource):
+    def __init__(self):
+        super().__init__()
+
+class Point(LightSource):
+    def __init__(self):
+        super().__init__()
+
+class DirectionalPoint(LightSource):
+    def __init__(self):
+        super().__init__()
