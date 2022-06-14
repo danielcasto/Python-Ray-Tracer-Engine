562842f157189b825193f9c24a6c9260adb43fbd
danielcasto <danielcasto242@gmail.com>
2022-06-13 22:42:54 -0700

    Declared functions for environment class and added requirements.txt

--- environment.py
+++ environment.py
@@ -1,3 +1,25 @@
 class Environment:
-    def __init__():
-        pass
+
+    def __init__(self, size):
+        # TODO
+        return self
+    
+    def addLightSource(self, light_type, intensity, pos, dir=None):
+        # TODO
+        return self
+
+    def addTriangle(self, v1, v2, v3, color):
+        # TODO
+        return self
+    
+    def addSphere(self, center, radius, color):
+        # TODO
+        return self
+    
+    def withParallelCamera(self, w, v, u, e):
+        # TODO
+        return self
+    
+    def withPerspectiveCamera(self, w, v, u, e, d):
+        # TODO
+        return self
