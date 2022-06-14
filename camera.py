761ebd54ff00deb0e133b9d8b5a017e97c227076
danielcasto <danielcasto242@gmail.com>
2022-06-13 23:12:02 -0700

    Added todo comments

--- camera.py
+++ camera.py
@@ -1,24 +1,30 @@
 class Camera:
     def __init__(self, w, v, u, e):
+        # TODO
         return self
 
     def set_basis(self, w, v, u, e):
+        # TODO
         return self
 
     def take_picture(self):
+        # TODO
         pass
 
 
 class ParallelCamera(Camera):
     def __init__(self, w, v, u, e):
         super().__init__(w, v, u, e)
+        # TODO
         return self
 
 
 class PerspectiveCamera(Camera):
     def __init__(self, w, v, u, e, d):
         super().__init__(w, v, u, e)
+        # TODO
         return self
 
     def set_basis(self, w, v, u, e, d):
+        # TODO
         return self
