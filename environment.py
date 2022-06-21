a94f6384b7123de22b2c6dfc39d3e8c5314c1904
danielcasto <danielcasto242@gmail.com>
2022-06-20 23:41:46 -0700

    Added pygame fundamentals to display given rgb array. Refactored Environment class to have snake_case instead of camelCase. Also, added use_camera to Environment to gather rgb array from camera.

--- environment.py
+++ environment.py
@@ -1,24 +1,28 @@
 class Environment:
     def __init__(self, size):
         # TODO currently unsupported
-        return self
+        pass
     
-    def addLightSource(self, light_type, intensity, pos, dir=None):
+    def add_light_source(self, light_type, intensity, pos, dir=None):
         # TODO currently unsupported
         return self
 
-    def addTriangle(self, v1, v2, v3, color):
+    def add_triangle(self, v1, v2, v3, color):
         # TODO currently unsupported
         return self
     
-    def addSphere(self, center, radius, color):
+    def add_sphere(self, center, radius, color):
         # TODO currently unsupported
         return self
     
-    def withParallelCamera(self, w, v, u, e):
+    def with_parallel_camera(self, w, v, u, e):
         # TODO currently unsupported
         return self
     
-    def withPerspectiveCamera(self, w, v, u, e, d):
+    def with_perspective_camera(self, w, v, u, e, d):
         # TODO currently unsupported
         return self
+    
+    def use_camera(self):
+        # TODO currently unsupported
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,61 @@
+import pygame as pg
+import numpy as np
+from environment import Environment
+from sys import exit
+
+SCREEN_DIMENSIONS = (1000, 600) # (width, height)
+
+def display_test():
+    pg.init()
+
+    test_surf = pg.Surface(SCREEN_DIMENSIONS)
+    TEST_RGB = np.full((1000, 600, 3), (255,0,0))
+
+    screen = pg.display.set_mode(SCREEN_DIMENSIONS, pg.RESIZABLE)
+    pg.display.set_caption('DISPLAY TEST')
+    program_is_alive = True
+    
+    pg.surfarray.blit_array(test_surf, TEST_RGB)
+
+    # game loop
+    while program_is_alive:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                program_is_alive = False
+
+        screen.blit(test_surf, (0,0))
+        pg.display.flip()
+        
+    pg.quit()
+    exit()
+
 def main():
-    pass
+    pg.init()
+    screen = pg.display.set_mode(SCREEN_DIMENSIONS, pg.RESIZABLE)
+    pg.display.set_caption('Python Ray Tracer Engine')
+    program_is_alive = True
+    surf = pg.Surface(SCREEN_DIMENSIONS)
+
+    env = Environment(SCREEN_DIMENSIONS)
+
+    rgb_arr = env.use_camera()
+    
+    pg.surfarray.blit_array(surf, rgb_arr)
+
+    # game loop
+    while program_is_alive:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                program_is_alive = False
+
+        screen.blit(surf, (0,0))
+        pg.display.flip()
+        
+    pg.quit()
+    exit()
+        
+
 
 if __name__ == '__main__':
     main()
+    # display_test()
