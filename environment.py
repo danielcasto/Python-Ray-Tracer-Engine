b51173dfacd2805a3b71448d93411d4e244e776e
danielcasto <danielcasto242@gmail.com>
2022-06-26 17:48:02 -0700

    Got rid of the redundant return types in Environment

--- environment.py
+++ environment.py
@@ -1,16 +1,15 @@
 from io import UnsupportedOperation
-from typing import Any
 from numpy import array
 from light_source import ConeLight, PointLight, DirectionalInfiniteLight
 from shapes import Triangle, Sphere
 
 class Environment:
-    def __init__(self, size: tuple[int, int]) -> None:
+    def __init__(self, size: tuple[int, int]):
         self.size: tuple[int, int] = size
         self.lights: list = []
         self.shapes: list = []
     
-    def add_light_source(self, light_type: str, intensity: float, pos: array = None, direction: array = None, angle: float = None) -> Any:
+    def add_light_source(self, light_type: str, intensity: float, pos: array = None, direction: array = None, angle: float = None):
         light_type_lowercase = light_type.lower()
 
         if light_type_lowercase == 'cone':
@@ -24,27 +23,27 @@ class Environment:
 
         return self
 
-    def add_triangle(self, v1: array, v2: array, v3: array, color: tuple[int, int, int]) -> Any:
+    def add_triangle(self, v1: array, v2: array, v3: array, color: tuple[int, int, int]):
         self.shapes.append(Triangle(v1, v2, v3, color))
 
         return self
     
-    def add_sphere(self, center: array, radius: float, color: tuple[int, int, int]) -> Any:
+    def add_sphere(self, center: array, radius: float, color: tuple[int, int, int]):
         self.shapes.append(Sphere(center, radius, color))
 
         return self
     
-    def with_parallel_camera(self, w: array, v: array, u: array, e: array) -> Any:
+    def with_parallel_camera(self, w: array, v: array, u: array, e: array):
         # TODO currently unsupported
         raise UnsupportedOperation
         return self
     
-    def with_perspective_camera(self, w: array, v: array, u: array, e: array, d: float) -> Any:
+    def with_perspective_camera(self, w: array, v: array, u: array, e: array, d: float):
         # TODO currently unsupported
         raise UnsupportedOperation
         return self
     
-    def use_camera(self) -> None:
+    def use_camera(self):
         # TODO currently unsupported
         raise UnsupportedOperation
         pass
